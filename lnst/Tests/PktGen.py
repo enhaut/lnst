65d99217e162c1a04654f49affb38ecb59b9feda
Samuel Dobron <sdobron@redhat.com>
2023-06-22 10:04:17 +0200

    PktGen: PacketGenerator test module
    PktGen is connection-less packet generator that
    simply "injects" packets to NIC's TX buffer.
    Because of that it requires `pktgen` kernel module
    if kernel wasn't compiled with `CONFIG_NET_PKTGEN`
    flag.
    
    Minimal example:
    ```python
    from lnst.Tests.PktGen import PktGen as P
    g = P(
          cpus=[0, 1, 2, 3],
          dst_mac="b4:83:51:FF:FF:FF",
          dst_ip="192.168.1.1",
          interface="eno12399"
    )
    g.run()
    print(g._res_data)
    ```
    
    Implementation is based on [1, 2, 3].
    
    [1] https://docs.kernel.org/networking/pktgen.html
    [2] https://github.com/torvalds/linux/blob/master/samples/pktgen
    [3] https://github.com/LNST-project/lnst/blob/legacy-py2/test_modules/PktgenTx.py

---
+++ lnst/Tests/PktGen.py
@@ -0,0 +1,186 @@
+import re
+import time
+import logging
+from subprocess import Popen
+from typing import Any, Iterator
+
+from lnst.Common.Utils import kmod_in_use
+from lnst.Common.IpAddress import Ip4Address
+from lnst.Tests.BaseTestModule import BaseTestModule, TestModuleError
+from lnst.Common.Parameters import IntParam, IpParam, StrParam, ListParam, DeviceParam
+
+
+class PktGenResultsParser:
+    def __init__(self, devs: list[str], res: dict) -> None:
+        self._devs = devs
+        self._res = res
+
+    def parse_dev_outputs(self):
+        for device, output in self._read_dev_outputs():
+            params, current, result = self._split_output(output)
+
+            self._res[device] = {
+                "params": self._parse_values(params),
+                "current": self._parse_values(current),
+                "result": self._parse_results(result),
+            }
+
+    def _read_dev_outputs(self) -> Iterator[tuple[str, str]]:
+        for device in self._devs:
+            output = ""
+            with open(f"/proc/net/pktgen/{device}", "r") as f:
+                output = f.readlines()
+            yield device, "\n".join(output)
+
+    def _split_output(self, output: str) -> tuple:
+        match = re.search(
+            r"Params:(.+)Current:(.+)Result:\s(?:\w+)(?::\s(.+))", output, re.DOTALL
+        )
+        if not match:
+            raise TestModuleError("Could not parse pktgen devide output")
+
+        return match.groups()
+
+    def _parse_values(self, params) -> dict[str, str]:
+        values = {}
+
+        for key, value in re.findall(r"(\w+):?\s(\S+)", params, re.MULTILINE):
+            values[key.lower()] = value
+
+        return values
+
+    def _parse_results(self, results) -> dict[str, Any]:
+        match = re.search(
+            r"(\d+).+ usec,\s(\d+)\s\(.+,(\d+)frags\).+?.+errors:\s?(\d+)",
+            results,
+            re.DOTALL,
+        )
+        if not match:
+            raise TestModuleError("Could not parse pktgen device result")
+
+        keys = ["duration", "packets", "frags", "errors"]
+
+        try:
+            results = dict(  # converts iterator to actual dict
+                zip(  # zips values with `keys`
+                    keys, map(int, match.groups())  # map converts str numbers to int
+                )
+            )
+        except ValueError:  # just to be sure all the nums can be converted to int
+            raise TestModuleError("Could not convert numbers in pktgen results")
+
+        results["duration"] /= 1_000_000  # from usec to sec
+        return results
+
+
+class PktGen(BaseTestModule):
+    """
+    In the scope of this module, the physical interface is refered as `interface`.
+    Pktgen device (interface@anything) is refered as device.
+
+    Inspired by https://github.com/torvalds/linux/blob/master/samples/pktgen/pktgen_sample03_burst_single_flow.sh
+    """
+
+    cpus = ListParam(type=IntParam())  # each CPU is 1 generator
+
+    src_if = DeviceParam()
+    dst_mac = StrParam()
+
+    src_ip = IpParam()
+    dst_ip = IpParam()
+
+    count = IntParam(mandatory=False, default=0)  # 0 = no upper limit
+    size = IntParam(mandatory=False, default=60)  # 4 bytes are added for CRC by NIC
+    frags = IntParam(mandatory=False, default=1)
+    burst = IntParam(mandatory=False, default=8)
+
+    duration = IntParam(default=60)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self._devices = []
+
+        self._res_data = {}
+        self._output_parser = None
+
+    def run(self):
+        if not kmod_in_use("pktgen"):
+            raise TestModuleError("pktgen module is not loaded")
+
+        logging.debug("Configuring PktGen generator")
+        self._pg_ctl("reset")
+        self._configure_generator()
+
+        output_parser = PktGenResultsParser(self._devices, self._res_data)
+
+        logging.debug("Starting generator")
+        pktgen = Popen("echo 'start' > /proc/net/pktgen/pgctrl", shell=True)
+        # ^^ echoing start to controller is blocking => needs to be separated
+
+        time.sleep(self.params.duration)
+
+        pktgen.kill()  # stops pktgen
+
+        output_parser.parse_dev_outputs()
+
+        self._deconfigure_generator()
+        return True
+
+    def _configure_generator(self):
+        logging.debug("Configuring generator")
+
+        ipv6 = False
+        if isinstance(self.params.src_ip, Ip4Address):
+            ipv6 = False
+        # breakpoint()
+
+        src = f"src{6 if ipv6 else ''}"
+        dest = f"dst{6 if ipv6 else ''}"
+
+        for cpu in self.params.cpus:
+            dev = f"{self.params.src_if.name}@{cpu}"
+            logging.debug(f"Adding interface {self.params.src_if.name} to cpu {cpu}")
+
+            self._pg_thread(cpu, f"add_device {dev}")
+
+            self._pg_set(cpu, f"flag QUEUE_MAP_CPU")
+            self._pg_set(cpu, f"count {self.params.count}")
+            self._pg_set(cpu, f"pkt_size {self.params.size}")
+            self._pg_set(cpu, f"flag NO_TIMESTAMP")
+
+            self._pg_set(cpu, f"dst_mac {self.params.dst_mac}")
+            self._pg_set(cpu, f"src_mac {self.params.src_if.hwaddr}")
+
+            self._pg_set(cpu, f"{dest}_min {self.params.dst_ip}")
+            self._pg_set(cpu, f"{dest}_max {self.params.dst_ip}")
+            self._pg_set(cpu, f"{src}_min {self.params.src_ip}")
+            self._pg_set(cpu, f"{src}_max {self.params.src_ip}")
+
+            self._pg_set(cpu, f"burst {self.params.burst}")
+            self._devices.append(dev)
+
+    def _deconfigure_generator(self):
+        logging.debug("Deconfiguring generator")
+        for cpu in self.params.cpus:
+            self._pg_thread(cpu, "rem_device_all")
+
+        self._pg_ctl("reset")
+
+    def _pg_ctl(self, cmd: str):
+        self._write_command("/proc/net/pktgen/pgctrl", cmd)
+
+    def _pg_thread(self, thread: int, cmd: str):
+        self._write_command(f"/proc/net/pktgen/kpktgend_{thread}", cmd)
+
+    def _pg_set(self, thread: int, cmd: str):
+        self._write_command(f"/proc/net/pktgen/{self.params.src_if.name}@{thread}", cmd)
+
+    def _write_command(self, file: str, cmd: str):
+        logging.debug(f"Writing {cmd} to {file}")
+        with open(file, "w") as f:
+            f.write(f"{cmd}\n")
+
+    def runtime_estimate(self):
+        return self.params.duration + 2
+

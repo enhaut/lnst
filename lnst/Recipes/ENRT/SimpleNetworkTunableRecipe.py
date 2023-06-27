7d7b1ff437e857d363ae12bbc5fb438f4c34d2ac
Jan Tluka <jtluka@redhat.com>
2023-06-27 11:04:35 +0200

    SimpleNetworkTunableRecipe: remove dev_interrupt_hw_config_dev_list property
    Signed-off-by: Jan Tluka <jtluka@redhat.com>

--- lnst/Recipes/ENRT/SimpleNetworkTunableRecipe.py
+++ lnst/Recipes/ENRT/SimpleNetworkTunableRecipe.py
@@ -105,7 +105,3 @@ class SimpleNetworkTunableRecipe(
     @property
     def coalescing_hw_config_dev_list(self):
         return [self.matched.host1.eth0, self.matched.host2.eth0]
-
-    @property
-    def dev_interrupt_hw_config_dev_list(self):
-        return [self.matched.host1.eth0, self.matched.host2.eth0]

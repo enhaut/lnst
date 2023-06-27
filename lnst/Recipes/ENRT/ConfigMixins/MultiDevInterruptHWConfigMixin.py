b1788f1659d84e4331e48d712046bdde4cc96619
Jan Tluka <jtluka@redhat.com>
2023-06-27 11:03:27 +0200

    MultiDevInterruptHWConfigMixin: specify host/device in multi_dev_interrupt_config parameter
    Signed-off-by: Jan Tluka <jtluka@redhat.com>

--- lnst/Recipes/ENRT/ConfigMixins/MultiDevInterruptHWConfigMixin.py
+++ lnst/Recipes/ENRT/ConfigMixins/MultiDevInterruptHWConfigMixin.py
@@ -1,4 +1,4 @@
-from lnst.Common.Parameters import ListParam, StrParam, ChoiceParam
+from lnst.Common.Parameters import DictParam
 from lnst.Recipes.ENRT.ConfigMixins.BaseHWConfigMixin import BaseHWConfigMixin
 from lnst.Recipes.ENRT.ConfigMixins.DevInterruptTools import pin_dev_interrupts
 
@@ -17,58 +17,66 @@ class MultiDevInterruptHWConfigMixin(BaseHWConfigMixin):
      .. note::
         Note that this Mixin also stops the irqbalance service.
 
-    :param dev_intr_cpu_lists:
-        (optional test parameter) each list specifies the CPU ids to which the
-        test device IRQs should be pinned, to skip the configuration for a device,
-        specify an empty list
-    :param dev_intr_cpu_policies:
-        (optional test parameter) for each test devices policy can be defined:
-        * all - pin each IRQ to all CPUs defined by the :attr:`dev_intr_cpu_lists`
-        * round-robin - use one CPU from the :attr:`dev_intr_cpu_lists` for each
-        test device IRQ, start from beginning if the number of IRQs is bigger
-        than the number of CPUs
+    :param multi_dev_interrupt_config:
+        (optional test parameter) this is a dictionary of per device IRQ binding
+        settings. The configuration is done per host per device, where each
+        device has a list of CPU ids and a policy specified.
+        The policy can be defined in following ways:
+            * "all" - pin each device IRQ to all CPUs in the cpus list
+            * "round-robin" - use one CPU from the cpus list for each
+            test device IRQ, start from beginning if the number of IRQs
+            is bigger than the number of CPUs
+
+        Example:
+        {
+            "host1": {
+                "eth1": {
+                    "cpus": [0,1],
+                    "cpu_policy": "all"
+                },
+            },
+            "host2": {
+                "eth1": {
+                    "cpus": [2,3],
+                    "cpu_policy": "round-robin"
+                },
+            }
+        }
     """
-
-    dev_intr_cpu_lists = ListParam(type=ListParam(), mandatory=False, default=[[]])
-    dev_intr_cpu_policies = ListParam(
-        type=ChoiceParam(type=StrParam, choices=set(["all", "round-robin"])),
-        mandatory=False,
-        default=[],
-    )
-
-    @property
-    def dev_interrupt_hw_config_dev_list(self):
-        """
-        The value of this property is a list of devices for which the IRQ CPU
-        affinity should be configured. It has to be defined by a derived class.
-        """
-        return []
+    multi_dev_interrupt_config = DictParam(mandatory=False)
 
     def hw_config(self, config):
         super().hw_config(config)
 
         hw_config = config.hw_config
 
-        if "dev_intr_cpu_lists" in self.params and any(self.params.dev_intr_cpu_lists):
+        device_settings = self._parse_device_settings(self.params.multi_dev_interrupt_config)
+        if device_settings:
             intr_cfg = hw_config["dev_intr_cpu_configuration"] = {}
             intr_cfg["irq_devs"] = {}
             intr_cfg["irqbalance_hosts"] = []
 
-            for dev, cpus, policy in zip(
-                self.dev_interrupt_hw_config_dev_list,
-                self.params.dev_intr_cpu_lists,
-                self.params.dev_intr_cpu_policies,
-            ):
-                if not cpus:
-                    continue
+        for dev, dev_config in device_settings.items():
+            cpus = dev_config["cpus"]
+            policy = dev_config["cpu_policy"]
 
-                if dev.host not in intr_cfg["irqbalance_hosts"]:
-                    dev.host.run("service irqbalance stop")
-                    intr_cfg["irqbalance_hosts"].append(dev.host)
+            if not cpus:
+                continue
 
-                # TODO better service handling through HostAPI
-                pin_dev_interrupts(dev, cpus, policy)
-                intr_cfg["irq_devs"][dev] = (cpus, policy)
+            if dev.host not in intr_cfg["irqbalance_hosts"]:
+                dev.host.run("service irqbalance stop")
+                intr_cfg["irqbalance_hosts"].append(dev.host)
+
+            # TODO better service handling through HostAPI
+            pin_dev_interrupts(dev, cpus, policy)
+            intr_cfg["irq_devs"][dev] = (cpus, policy)
+
+    def hw_deconfig(self, config):
+        intr_config = config.hw_config.get("dev_intr_cpu_configuration", {})
+        for host in intr_config.get("irqbalance_hosts", []):
+            host.run("service irqbalance start")
+
+        super().hw_deconfig(config)
 
     def describe_hw_config(self, config):
         desc = super().describe_hw_config(config)

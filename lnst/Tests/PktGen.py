e60e703ad283d2b80f3399b0851b827f5ccaef64
Samuel Dobron <sdobron@redhat.com>
2023-07-11 13:05:24 +0200

    device param refactor

--- lnst/Tests/PktGen.py
+++ lnst/Tests/PktGen.py
@@ -5,9 +5,9 @@ from typing import Iterator
 from subprocess import Popen
 
 from lnst.Common.Utils import kmod_in_use
-from lnst.Common.IpAddress import ipaddress, Ip4Address
+from lnst.Common.IpAddress import Ip4Address
 from lnst.Tests.BaseTestModule import BaseTestModule, TestModuleError
-from lnst.Common.Parameters import IntParam, IpParam, StrParam, ListParam
+from lnst.Common.Parameters import IntParam, StrParam, ListParam, DeviceParam
 
 
 class PktGenResultsParser:
@@ -60,15 +60,8 @@ class PktGen(BaseTestModule):
 
     pgctrl = StrParam(mandatory=False, default="/proc/net/pktgen/pgctrl")
 
-    src_mac = StrParam(mandatory=False, default="90:e2:ba:ff:ff:ff")
-    dst_mac = StrParam()
-
-    src_ip = IpParam(
-        mandatory=False, default=ipaddress("192.168.1.1")
-    )  # IP doesn't really matter
-    dst_ip = IpParam()
-
-    interface = StrParam()
+    src_if = DeviceParam()
+    dst_if = DeviceParam()
 
     count = IntParam(mandatory=False, default=0)  # 0 = no upper limit
     size = IntParam(mandatory=False, default=60)  # 4 bytes are added for CRC by NIC
@@ -80,21 +73,14 @@ class PktGen(BaseTestModule):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        if not kmod_in_use("pktgen"):
-            raise TestModuleError("pktgen module is not loaded")
-
         self._devices = []
         self._ip6 = True
-        if isinstance(self.params.dst_ip, Ip4Address):
+        if isinstance(self.params.dst_if.ips[0], Ip4Address):
             self._ip6 = False
 
         self._res_data = {}
-        self._output_parser = PktGenResultsParser(self._devices, self._res_data)
-
-        self._pg_ctl("reset")
-
-        self._configure_generator()
-
+        self._output_parser = None
+        
     def _write_command(self, file: str, cmd: str):
         with open(file, "w") as f:
             f.write(cmd + "\n")
@@ -113,6 +99,9 @@ class PktGen(BaseTestModule):
         src = f"src{6 if self._ip6 else ''}"
         dest = f"dst{6 if self._ip6 else ''}"
 
+        src_ip = self.params.src_if.ips[0]  # xdp uses just 1 IP
+        dst_ip = self.params.dst_if.ips[0]
+
         for cpu in self.params.cpus:
             dev = f"{self.params.interface}@{cpu}"
             logging.debug(f"Adding interface {self.params.interface} to cpu {cpu}")
@@ -124,13 +113,13 @@ class PktGen(BaseTestModule):
             self._pg_set(cpu, f"pkt_size {self.params.size}")
             self._pg_set(cpu, f"flag NO_TIMESTAMP")
 
-            self._pg_set(cpu, f"dst_mac {self.params.dst_mac}")
-            self._pg_set(cpu, f"src_mac {self.params.src_mac}")
+            self._pg_set(cpu, f"dst_mac {self.params.dst_if.hwaddr}")
+            self._pg_set(cpu, f"src_mac {self.params.src_if.hwaddr}")
 
-            self._pg_set(cpu, f"{dest}_min {self.params.dst_ip}")
-            self._pg_set(cpu, f"{dest}_max {self.params.dst_ip}")
-            self._pg_set(cpu, f"{src}_min {self.params.src_ip}")
-            self._pg_set(cpu, f"{src}_max {self.params.src_ip}")
+            self._pg_set(cpu, f"{dest}_min {src_ip}")
+            self._pg_set(cpu, f"{dest}_max {dst_ip}")
+            self._pg_set(cpu, f"{src}_min {src_ip}")
+            self._pg_set(cpu, f"{src}_max {dst_ip}")
 
             self._pg_set(cpu, f"burst {self.params.burst}")
             self._devices.append(dev)
@@ -142,7 +131,16 @@ class PktGen(BaseTestModule):
 
         self._pg_ctl("reset")
 
-    def run(self):
+    def run(self): 
+        if not kmod_in_use("pktgen"):
+            raise TestModuleError("pktgen module is not loaded")
+        
+        logging.debug("Configuring PktGen generator")
+        self._pg_ctl("reset")
+        self._configure_generator()
+        
+        output_parser = PktGenResultsParser(self._devices, self._res_data)
+        
         logging.debug("Starting generator")
         pktgen = Popen("echo 'start' > /proc/net/pktgen/pgctrl", shell=True)
         # ^^ echoing start to controller is blocking => needs to be separated
@@ -151,7 +149,8 @@ class PktGen(BaseTestModule):
 
         pktgen.kill()  # stops pktgen
 
-        self._output_parser.parse_dev_outputs()
+        output_parser.parse_dev_outputs()
 
         self._deconfigure_generator()
         return True
+

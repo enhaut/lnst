4be536d0cd4065c756a7efa0e153940886a93d0d
Samuel Dobron <sdobron@redhat.com>
2025-01-12 23:47:50 +0100

    WIP: fwd

---
+++ lnst/RecipeCommon/Perf/Measurements/ForwardingMeasurement.py
@@ -0,0 +1,74 @@
+from .XDPBenchMeasurement import XDPBenchMeasurement
+from lnst.Controller.Recipe import BaseRecipe
+from lnst.RecipeCommon.Perf.Measurements.Results.AggregatedXDPBenchMeasurementResults import (
+    AggregatedXDPBenchMeasurementResults,
+)
+from lnst.Controller.RecipeResults import ResultType
+from lnst.RecipeCommon.Perf.Measurements.BaseFlowMeasurement import (
+    Flow,
+    NetworkFlowTest,
+)
+from lnst.RecipeCommon.Perf.Measurements.MeasurementError import MeasurementError
+from lnst.RecipeCommon.Perf.Measurements.Results.XDPBenchMeasurementResults import (
+    XDPBenchMeasurementResults,
+)
+from lnst.RecipeCommon.Perf.Results import (
+    PerfInterval,
+    ParallelPerfResult,
+    SequentialPerfResult,
+)
+from lnst.Tests.PktGen import PktGen
+from lnst.Tests.XDPBench import XDPBench
+from lnst.Controller.Job import Job
+from lnst.Controller.RecipeResults import MeasurementResult, ResultType
+from lnst.RecipeCommon.Perf.Measurements.BaseFlowMeasurement import BaseFlowMeasurement
+
+
+
+class ForwardingMeasurement(XDPBenchMeasurement):
+    def __init__(self, flows, recipe_conf=None):
+        super().__init__(flows, "drop", "skb", recipe_conf=recipe_conf)
+
+    def _prepare_forwarder(self, flow):
+        pass
+
+    def _prepare_server(self, flow):
+        params = {
+            "command": "drop",
+            "xdp_mode": self.mode,
+            "interface": flow.target_nic,
+            "duration": flow.duration + flow.warmup_duration * 2,
+        }
+        bench = XDPBench(**params)
+        job = flow.target.prepare_job(bench)
+
+        return job
+
+    def _prepare_client(self, flow: Flow):
+        params = {
+            "src_if": flow.generator_nic,
+            "dst_mac": flow.receiver_nic.hwaddr,
+            "src_ip": flow.generator_bind,
+            "dst_ip": flow.receiver_bind,
+            "cpus": [0],# flow.generator_cpupin,
+            "pkt_size": flow.msg_size,
+            "duration": flow.duration + flow.warmup_duration * 2,
+            "src_port": flow.generator_port,
+            "dst_port": flow.receiver_port,
+        }
+        pktgen = PktGen(**params)
+
+        job = flow.generator.prepare_job(pktgen)
+
+        return job
+
+    def _prepare_flows(self) -> list[NetworkFlowTest]:
+        flows = []
+        for flow in self.flows:
+            client = self._prepare_client(flow)
+            # forwarder = self._prepare_forwarder(flow)
+            server = self._prepare_server(flow)
+            net_flow = NetworkFlowTest(flow, server, client)
+            flows.append(net_flow)
+
+        return flows
---
+++ lnst/Recipes/ENRT/MeasurementGenerators/ForwardingMeasurementGenerator.py
@@ -0,0 +1,49 @@
+from lnst.Common.Parameters import ChoiceParam, StrParam
+from lnst.RecipeCommon.Perf.Measurements.XDPBenchMeasurement import XDPBenchMeasurement
+from lnst.Recipes.ENRT.MeasurementGenerators.BaseFlowMeasurementGenerator import (
+    BaseFlowMeasurementGenerator,
+)
+import itertools
+from lnst.RecipeCommon.Perf.Measurements.ForwardingMeasurement import ForwardingMeasurement
+
+
+class ForwardingMeasurementGenerator(BaseFlowMeasurementGenerator):
+    @property
+    def net_perf_tool_class(self):
+        def ForwardingMeasurement_partial(*args, **kwargs):
+            return ForwardingMeasurement(*args, **kwargs)
+
+        return ForwardingMeasurement_partial
+    
+    def _create_perf_flows(
+        self,
+        endpoint_pairs,
+        perf_test: str,
+        msg_size,
+    ) -> list:
+        port_iter = itertools.count(12000)
+
+        flows = []
+        for endpoint_pair in endpoint_pairs:
+            client, server = endpoint_pair
+            for i in range(1):  # TODO:vymenit za perf_parallel_processes, potom je cela tato funkcia zbytocna a moze sa pouzit parent implementacia
+
+                server_port = client_port = next(port_iter)
+                flow = self._create_perf_flow(
+                    perf_test,
+                    client.device,
+                    client.address,
+                    client_port if perf_test != "mptcp_stream" else None,
+                    server.device,
+                    server.address,
+                    server_port,
+                    msg_size,
+                    self.generator_cpupin(i),
+                    self.receiver_cpupin(i),
+                )
+                flow.target = self.matched.host1
+                flow.target_nic = self.matched.host1.eth1
+
+                flows.append(flow)
+
+        return flows

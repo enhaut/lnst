c51d88db3e24c694ba35652bd62fede868b7e2b8
Samuel Dobron <sdobron@redhat.com>
2025-01-14 01:01:13 +0100

    forwarding recipe: using netspace

--- lnst/Recipes/ENRT/ForwardingRecipe.py
+++ lnst/Recipes/ENRT/ForwardingRecipe.py
@@ -16,9 +16,13 @@ import itertools
 from lnst.Common.IpAddress import Ip4Address, Ip6Address
 from lnst.Devices import RemoteDevice
 from lnst.RecipeCommon.endpoints import EndpointPair, IPEndpoint
+from lnst.Recipes.ENRT.ConfigMixins.MultiDevInterruptHWConfigMixin import (
+    MultiDevInterruptHWConfigMixin,
+)
 
+from lnst.Controller.NetNamespace import NetNamespace
 
-class ForwardingRecipe(ForwardingMeasurementGenerator, BaremetalEnrtRecipe):
+class ForwardingRecipe(MultiDevInterruptHWConfigMixin, ForwardingMeasurementGenerator, BaremetalEnrtRecipe):
     host1 = HostReq()
     host1.eth0 = DeviceReq(label="net1", driver=RecipeParam("driver"))
     host1.eth1 = DeviceReq(label="net1", driver=RecipeParam("driver2"))
@@ -59,29 +63,27 @@ class ForwardingRecipe(ForwardingMeasurementGenerator, BaremetalEnrtRecipe):
         egress6 = interface_addresses(egress6_net)
         ingress6 = interface_addresses(ingress6_net)
 
+        host1.receiver_ns = NetNamespace("lnst-receiver_ns")
+        host1.receiver_ns.eth1 = host1.eth1
+        host1.receiver_ns.run("ip link set dev lo up")
+
         for host in [host1, host2]:
             config.configure_and_track_ip(host.eth0, next(egress4))
             config.configure_and_track_ip(host.eth0, next(egress6))
             host.eth0.up_and_wait()
 
-            config.configure_and_track_ip(host.eth1, next(ingress4))
-            config.configure_and_track_ip(host.eth1, next(ingress6))
-            host.eth1.up_and_wait()
+        config.configure_and_track_ip(host1.receiver_ns.eth1, next(ingress4))
+        config.configure_and_track_ip(host1.receiver_ns.eth1, next(ingress6))
+        host1.receiver_ns.eth1.up_and_wait()
+
+        config.configure_and_track_ip(host2.eth1, next(ingress4))
+        config.configure_and_track_ip(host2.eth1, next(ingress6))
+        host2.eth1.up_and_wait()
 
         host2.run("echo 1 > /proc/sys/net/ipv4/ip_forward")
         host2.run("echo 1 > /proc/sys/net/ipv6/conf/all/forwarding")
 
         self.wait_tentative_ips(config.configured_devices)
-
-        host2.run(
-            f"ip neigh add {filter_ip(host1.eth1, AF_INET)} lladdr {host1.eth1.hwaddr} dev {host2.eth1.name}"
-        )
-        host2.run(
-            f"ip -6 neigh add {filter_ip(host1.eth1, AF_INET6)} lladdr {host1.eth1.hwaddr} dev {host2.eth1.name}"
-        )
-        # NOTE: needs to add neighs manually; generator ingress interface runs XDP drop,
-        # so it doesn't respond to ARP/NDP requests from receiver egress interface
-        # when routing traffic to ingress network of generator
 
         minimal_prefix_len = max(
             1, math.ceil(math.log2(self.params.perf_parallel_streams))
@@ -95,28 +97,25 @@ class ForwardingRecipe(ForwardingMeasurementGenerator, BaremetalEnrtRecipe):
             zip(routed4, routed6)
         )  # needs to be list, not generator. It's used multiple times
         for net4, net6 in self.routed:
+            config.configure_and_track_ip(host1.receiver_ns.eth1, Ip4Address(f"{net4[1]}/{net4.prefixlen}"))
+            config.configure_and_track_ip(host1.receiver_ns.eth1, Ip6Address(f"{net6[1]}/{net6.prefixlen}"))
+            config.configure_and_track_ip(host2.eth1, Ip4Address(f"{net4[2]}/{net4.prefixlen}"))
+            config.configure_and_track_ip(host2.eth1, Ip6Address(f"{net6[2]}/{net6.prefixlen}"))
 
-            host2.run(
-                f"ip neigh add {net4[1]} lladdr {host1.eth1.hwaddr} dev {host2.eth1.name}"
-            )
-            host2.run(
-                f"ip -6 neigh add {net6[1]} lladdr {host1.eth1.hwaddr} dev {host2.eth1.name}"
-            )
-            # ^ net{4,6}[1]: IPvxNetwork.__getitem__ returns list of ALL IPs (including network IP), so first host is at [1]
+            host1.run(f"ip route add {net4} via {filter_ip(host2.eth0, AF_INET)} dev {host1.eth0.name}")
+            host1.run(f"ip -6 route add {net6} via {filter_ip(host2.eth0, AF_INET6)} dev {host1.eth0.name}")
 
-            host1.run(
-                f"ip route add {net4} via {filter_ip(host2.eth0, AF_INET)} dev {host1.eth0.name}"
-            )
-            host1.run(
-                f"ip route add {net6} via {filter_ip(host2.eth0, AF_INET6)} dev {host1.eth0.name}"
-            )
+            # MacOS specific workaround
+            # Sometimes happen that IPv6 networking doesn't work for couple of seconds
+            # no idea why, the network is just not reachable.
+            # This removes ndp entry and for some reason when the connection is restored
+            # it doesn't ask for new NDP.
+            # TODO: check if present on x86
+            host2.run(f"ip -6 neigh add {net6[1]} lladdr {host1.receiver_ns.eth1.hwaddr} dev {host2.eth1.name}")
 
-            host2.run(
-                f"ip route add {net4} via {filter_ip(host1.eth1, AF_INET)} dev {host2.eth1.name}"
-            )
-            host2.run(
-                f"ip route add {net6} via {filter_ip(host1.eth1, AF_INET6)} dev {host2.eth1.name}"
-            )
+        # setup default routes in receiver namespace to enable communication TO outside
+        host1.receiver_ns.run(f"ip route add 0.0.0.0/0 via {self.routed[0][0][2]} dev {host1.receiver_ns.eth1.name}")
+        host1.receiver_ns.run(f"ip -6 route add ::/0 via {self.routed[0][1][2]} dev {host1.receiver_ns.eth1.name}")
 
         return config
 
@@ -127,17 +126,13 @@ class ForwardingRecipe(ForwardingMeasurementGenerator, BaremetalEnrtRecipe):
         host2.run("echo 0 > /proc/sys/net/ipv4/ip_forward")
         host2.run("echo 0 > /proc/sys/net/ipv6/conf/all/forwarding")
 
-        # remove ingress network manually added neighs:
-        for ip in config._device_ips[host1.eth1]:
-            host2.run(f"ip neigh del {ip} dev {host2.eth1.name}")
-
         # remove routes and neighs for routed networks:
         for net4, net6 in self.routed:
             for host in [host1, host2]:
                 host.run(f"ip route del {net4}")
                 host.run(f"ip route del {net6}")
 
-            host2.run(f"ip neigh del {net4[1]} dev {host2.eth1.name}")
+            # MacOS specific workaround
             host2.run(f"ip -6 neigh del {net6[1]} dev {host2.eth1.name}")
 
         return config
@@ -164,7 +159,8 @@ class ForwardingRecipe(ForwardingMeasurementGenerator, BaremetalEnrtRecipe):
             [PingEndpoints(self.matched.host1.eth0, self.matched.host2.eth0)]
         """
         return [PingEndpoints(self.matched.host1.eth0, self.matched.host2.eth0),
-                PingEndpoints(self.matched.host2.eth1, self.matched.host1.eth1)]
+                PingEndpoints(self.matched.host2.eth1, self.matched.host1.receiver_ns.eth1)]#,
+        #PingEndpoints(self.matched.host1.eth0, self.matched.host1.receiver_ns.eth1)]  # TODO:
 
     def generate_perf_endpoints(
         self, config: EnrtConfiguration
@@ -173,11 +169,20 @@ class ForwardingRecipe(ForwardingMeasurementGenerator, BaremetalEnrtRecipe):
         Function generates endpoints pairs where flow goes
         from host1.eth0 to host2.eth0. host2 then redirects
         traffic back to host1.eth1.
+
+        Pktgen doesn't do any lookup for MAC based on IP,
+        so this function needs to set destination device
+        forwarder NIC (because it's MAC is used in PktGen)
+        BUT destination IP is set to regular destination.
+
+        This is similar to what PC usually do, if it
+        receives packet to some other net, it'll set IP
+        to the destination and forward it to the next hop,
+        which is in this case forwarder (host2).
         """
         endpoint_pairs = []
         dev1 = self.matched.host1.eth0
         dev2 = self.matched.host2.eth0
-        target_dev = self.matched.host1.eth1
 
         for ip_type in [Ip4Address, Ip6Address]:
             dev1_ips = [ip for ip in config.ips_for_device(dev1) if isinstance(ip, ip_type)]

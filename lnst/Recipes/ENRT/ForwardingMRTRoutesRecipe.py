fad5b1568717a1900e2e9dafc7d8ca493b67c9a5
Samuel Dobron <sdobron@redhat.com>
2025-02-23 22:49:13 +0100

    Recipes: added MTR routes recipes
    Recipe extends basic forwarding recipe but adds support
    for adding MTR routes to routing table on forwarding machine
    which may be used for FIB table stress.

---
+++ lnst/Recipes/ENRT/ForwardingMRTRoutesRecipe.py
@@ -0,0 +1,6 @@
+from lnst.Recipes.ENRT.MTRRoutesRecipe import MTRRoutesRecipe
+from lnst.Recipes.ENRT.ForwardingRecipe import ForwardingRecipe
+
+
+class ForwardingMRTRoutesRecipe(MTRRoutesRecipe, ForwardingRecipe):
+    pass

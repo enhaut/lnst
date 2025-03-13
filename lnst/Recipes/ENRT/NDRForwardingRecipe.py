161fe952fe6b5d23b52e20e165d150e0719c12ce
Samuel Dobron <sdobron@redhat.com>
2025-03-13 12:10:33 +0100

    sq no drop forwarding recipe

--- lnst/Recipes/ENRT/NDRForwardingRecipe.py
+++ lnst/Recipes/ENRT/NDRForwardingRecipe.py
@@ -3,4 +3,5 @@ from .ForwardingRecipe import ForwardingRecipe
 
 
 class NDRForwardingRecipe(NoDropRateMixin, ForwardingRecipe):
-    pass
+    def _get_nic_to_watch(self, flow):
+        return self.matched.host2.eth1  # forwarding egress inf

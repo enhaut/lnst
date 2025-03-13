ad16f5ba24fb6b10fbe2d10672cde02f9148dbbc
Samuel Dobron <sdobron@redhat.com>
2025-03-13 20:12:02 +0100

    sq ndr forwarding recipe

--- lnst/Recipes/ENRT/NDRXDPForwardingRecipe.py
+++ lnst/Recipes/ENRT/NDRXDPForwardingRecipe.py
@@ -3,5 +3,4 @@ from .XDPForwardingRecipe import XDPForwardingRecipe
 
 
 class NDRXDPForwardingRecipe(NoDropRateMixin, XDPForwardingRecipe):
-    def _get_nic_to_watch(self, flow):
-        return self.matched.host2.eth1  # forwarding egress inf
+    pass

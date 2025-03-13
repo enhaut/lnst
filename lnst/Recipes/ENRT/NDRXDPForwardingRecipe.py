bf5c9d2b6877320590a5a1e41ac84313c56755d2
Samuel Dobron <sdobron@redhat.com>
2025-03-13 12:10:41 +0100

    no drop xdp forwarding recipe

--- lnst/Recipes/ENRT/NDRXDPForwardingRecipe.py
+++ lnst/Recipes/ENRT/NDRXDPForwardingRecipe.py
@@ -3,4 +3,5 @@ from .XDPForwardingRecipe import XDPForwardingRecipe
 
 
 class NDRXDPForwardingRecipe(NoDropRateMixin, XDPForwardingRecipe):
-    pass
+    def _get_nic_to_watch(self, flow):
+        return self.matched.host2.eth1  # forwarding egress inf
